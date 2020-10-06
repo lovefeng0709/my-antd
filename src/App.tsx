48e1858ae66ad4641986e925b490705d7146833a
zujiaozhang <859166618@qq.com>
2020-10-06 22:36:22 +0800

    1006

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button,{ButtonSize,ButtonType} from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large} disabled>按钮</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large} >按钮</Button>
+        <Button  size={ButtonSize.Small} >按钮</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small} >按钮</Button>
+        <Button btnType={ButtonType.Link}  href="https://github.com" size={ButtonSize.Large}></Button>
+        <Button btnType={ButtonType.Link}  href="https://github.com" size={ButtonSize.Large} disabled></Button>
+        <h1>hello world</h1>
+        <h2>hello world</h2>
+        <h3>hello world</h3>
+        <code>
+          const a = 1;
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
