622fd25790a569f99639621191666e129bd33409
zujiaozhang <859166618@qq.com>
2020-12-06 21:38:14 +0800

    1206

---
+++ src/hooks/useClickOutside.tsx
@@ -0,0 +1,14 @@
+import {RefObject,useEffect} from 'react';
+function useClickOutside(ref: RefObject<HTMLDivElement>,handler: Function){
+    useEffect(() => {
+        const listener = (event: MouseEvent)=>{
+            if(!ref.current||ref.current.contains(event.target as HTMLElement)){
+                return
+            }
+            handler(event)
+        }
+        document.addEventListener('click',listener)
+        return ()=> document.removeEventListener('click',listener)
+    })
+}
+export default useClickOutside
