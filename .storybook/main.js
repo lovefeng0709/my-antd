33107f71751a3d20ae1535336ef6e1012150a0ae
zujiaozhang <859166618@qq.com>
2020-12-16 22:16:25 +0800

    again

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@
  * @Author: love-coding
  * @Date: 2020-11-08 19:40:29
  * @LastEditors: love-coding
- * @LastEditTime: 2020-12-16 21:52:56
+ * @LastEditTime: 2020-12-16 22:15:16
  */
 module.exports = {
   "stories": [
@@ -15,7 +15,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
-    "@storybook/preset-scss",
+    // "@storybook/preset-scss",
   ],
   typescript: {
     check: false,
---
+++ storybook-static/precache-manifest.c8a12c2663420178c8902e9f0b8c2cf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9f6af003cf8d112c493dcc69ec1b75b",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.8664d2e21cfd099e9903.bundle.js"
+  },
+  {
+    "url": "runtime~main.8664d2e21cfd099e9903.bundle.js"
+  },
+  {
+    "revision": "95f45490c860767e35dc",
+    "url": "static/css/main.5ce6c761.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.8664d2e21cfd099e9903.bundle.js"
+  },
+  {
+    "url": "vendors~main.8664d2e21cfd099e9903.bundle.js.LICENSE.txt"
+  }
+]);
